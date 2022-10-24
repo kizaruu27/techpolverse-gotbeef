452af26d79d8174fd457481284cbf6da6089aa92
shantaufiq <shantaufiq@student.telkomuniversity.ac.id>
2022-10-24 11:02:39 +0700

    Fix: progress bar balance cooking

--- Techpolverse - GotBeef/Assets/_MAIN/_Scripts/Cooking Mechanism V2/View/CookingTimer.cs
+++ Techpolverse - GotBeef/Assets/_MAIN/_Scripts/Cooking Mechanism V2/View/CookingTimer.cs
@@ -1,45 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class CookingTimer : MonoBehaviour
 {
-  public bool isPointerInArea;
-  public bool isDead;
-  public bool isFinish;
-  public float time;
-  public float timeInArea;
-  [SerializeField] float _deadTime;
-  [SerializeField] float _finishTime;
+    public bool isPointerInArea;
+    public bool isDead;
+    public bool isFinish;
+    public float time;
+    public float timeInArea;
+    [SerializeField] float _deadTime;
+    [SerializeField] float _finishTime;
 
-  public TextMeshProUGUI timeUI;
+    public TextMeshProUGUI timeUI;
 
-  private void Update()
-  {
-    GetTime();
-    ShowTime();
-    InGameFinish();
-  }
+    public Slider _ProgressBar;
+
+    private void Update()
+    {
+        GetTime();
+        ShowTime();
+        InGameFinish();
+    }
 
 
-  void GetTime()
-  {
-    timeInArea = isPointerInArea ?
-      timeInArea + (Time.deltaTime * 1f) :
-      timeInArea > 0 ?
-        timeInArea - (Time.deltaTime * 1f) : 0;
+    void GetTime()
+    {
+        timeInArea = isPointerInArea ?
+          timeInArea + (Time.deltaTime * 1f) :
+          timeInArea > 0 ?
+            timeInArea - (Time.deltaTime * 1f) : 0;
 
-    time += Time.deltaTime * 1f;
-  }
+        time += Time.deltaTime * 1f;
 
-  void ShowTime()
-  {
-    timeUI.text = timeInArea.ToString("0.00") + " Detik";
-  }
+        _ProgressBar.value = timeInArea;
+    }
 
-  void InGameFinish()
-  {
-    isDead = time >= _deadTime;
-    isFinish = timeInArea >= _finishTime;
-  }
+    void ShowTime()
+    {
+        timeUI.text = timeInArea.ToString("0.00") + " Detik";
+    }
+
+    void InGameFinish()
+    {
+        isDead = time >= _deadTime;
+        isFinish = timeInArea >= _finishTime;
+    }
 
 }
