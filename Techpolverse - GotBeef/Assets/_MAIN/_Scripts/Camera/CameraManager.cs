f0f23a74d5d72cd46b8b15ce40664f3f68962b81
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2022-11-02 14:48:47 +0700

    modified: camera transition

--- Techpolverse - GotBeef/Assets/_MAIN/_Scripts/Camera/CameraManager.cs
+++ Techpolverse - GotBeef/Assets/_MAIN/_Scripts/Camera/CameraManager.cs
@@ -8,21 +8,36 @@ public class CameraManager : MonoBehaviour
     [SerializeField] Transform cam;
     [SerializeField] Transform normalPos;
     [SerializeField] Transform cookingPos;
+    [SerializeField] Transform cookingTarget;
+
+    private bool isCooking;
 
     private void Start()
     {
        SetCameraToNormal();
     }
 
+    private void FixedUpdate()
+    {
+        if (isCooking)
+        {
+            cam.position = Vector3.Lerp(cam.position, cookingPos.position, .5f);
+            cam.LookAt(cookingTarget);
+        }
+        else
+        {
+            cam.position = Vector3.Lerp(cam.position, normalPos.position, .5f);
+            cam.rotation = normalPos.rotation;
+        }
+    }
+
     public void SetCameraToNormal()
     {
-        cam.position = normalPos.position;
-        cam.rotation = normalPos.rotation;
+        isCooking = false;
     }
 
     public void SetCameraToCookingMode()
     {
-        cam.position = cookingPos.position;
-        cam.rotation = cookingPos.rotation;
+        isCooking = true;
     }
 }
